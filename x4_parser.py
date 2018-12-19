88c9849cdddb82af7a49d13dac6ed6edd5d99598
Russ Willis <russ.willis@digital.landregistry.gov.uk>
2018-12-19 13:42:26 +0000

    Start to implement a JSON output function

--- x4_parser.py
+++ x4_parser.py
@@ -37,7 +37,7 @@ def main():
             with open('extracted_logs.csv', 'w') as extracted_file:
                 # Have to go out of the way to actually kill the UI bit otherwise it hangs about forever and
                 # process won't run - learn threading?
-                main_window.destroy()
+                # main_window.destroy()
                 for elem in get_elements(gzipped_file, 'entry'):
                     match = pattern.match(elem.attrib['text'])
                     if match:
@@ -45,18 +45,40 @@ def main():
                                                            match.group(4))
                         extracted_file.writelines(csv_line)
                         extracted_file.write('\n')
+                        response_text.delete("1.0", END)
+                        response_text.insert(END, 'CSV file generated')
+
+    def generate_json():
+        with gzip.open(path_actual_entry.get(), 'rt') as gzipped_file:
+            with open('extracted_logs.csv', 'w') as extracted_file:
+                # Have to go out of the way to actually kill the UI bit otherwise it hangs about forever and
+                # process won't run - learn threading?
+                # main_window.destroy()
+                for elem in get_elements(gzipped_file, 'entry'):
+                    match = pattern.match(elem.attrib['text'])
+                    if match:
+                        response_text.delete("1.0", END)
+                        response_text.insert(END, 'JSON file generated')
+
 
     # Create widgets
     path_label = Label(app, text='Path to save file')
     browse_button = Button(app, text="Browse", command=browse_function)
-    go_button = Button(app, text='Start generating', command=generate_csv)
-    path_actual_entry = Entry(app, width=100)
+    csv_button = Button(app, text='Generate CSV', command=generate_csv)
+    json_button = Button(app, text='Generate JSON', command=generate_json)
+    path_actual_entry = Entry(app, width=50)
+    response_text = Text(app, height=2, width=50)
 
     # Layout grid
     path_label.grid(row=1, column=0)
     browse_button.grid(row=2, column=0)
     path_actual_entry.grid(row=2, column=1, columnspan=3, sticky=E+W)
-    go_button.grid(row=3, column=1, rowspan=2)
+    csv_button.grid(row=3, column=1)
+    json_button.grid(row=3, column=2)
+    response_text.grid(row=4, column=1)
+
+    # Initial setup
+    response_text.insert(END, 'Results go here')
 
     app.mainloop()
 
