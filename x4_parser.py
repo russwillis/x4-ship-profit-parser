45214c43fa77dac9672f2c84a84bff38f97bee31
Russ Willis <russ.willis@digital.landregistry.gov.uk>
2019-01-16 08:41:46 +0000

    Work more on the formatting for onscreen view

--- x4_parser.py
+++ x4_parser.py
@@ -17,7 +17,7 @@ class X4(Frame):
 
 def main():
     main_window = Tk()
-    main_window.geometry('800x600')
+    main_window.geometry('1200x800')
     app = X4(master=main_window)
     app.master.title('X4 Save file parser')
 
@@ -74,7 +74,6 @@ def main():
                 for elem in get_elements(gzipped_file, 'entry'):
                     match = pattern.match(elem.attrib['text'])
                     if match:
-                        # code to go here to generate some json :)
                         if match.group(1) in reformatted_text:
                             reformatted_text[match.group(1)]['cash'] = reformatted_text[match.group(1)]['cash'] + int(
                                 match.group(4))
@@ -85,28 +84,60 @@ def main():
                 # Update the status text field in GUI by removing old message and supplying new
                 response_text.delete("1.0", END)
                 response_text.insert(END, 'List generated')
+                # Now prep the data ready for display on screen
                 all_keys = list(reformatted_text.keys())
-                ship_list_box = Listbox(app, width=60, height=20)
-                for i in range(len(reformatted_text)):
+
+                for i in range(len(all_keys)):
                     # format the prices to have comma separators
                     cash = format(int(reformatted_text[all_keys[i]]['cash']), ',d')
+                    trip_count = int(reformatted_text[all_keys[i]]['count'])
                     average_cash = format(int(reformatted_text[all_keys[i]]['cash']) //
                                           int(reformatted_text[all_keys[i]]['count']), ',d')
-                    ship_list_box.insert(END, 'Credits:  {}||  Average: {} || Ship: {}'.format
-                    (cash, average_cash, all_keys[i]))
-                ship_list_box.grid(row=5, column=1)
+
+                    # try to make a nice grid style
+                    ship_name = Entry(app, justify=LEFT)
+                    ship_cash = Entry(app, justify=RIGHT)
+                    ship_average = Entry(app, justify=RIGHT)
+                    ship_trips = Entry(app, justify=CENTER)
+                    ship_name.delete(0, END)
+                    ship_cash.delete(0, END)
+                    ship_average.delete(0, END)
+                    ship_trips.delete(0, END)
+                    ship_name.insert(0, all_keys[i])
+                    ship_cash.insert(0, cash)
+                    ship_average.insert(0, average_cash)
+                    ship_trips.insert(0, trip_count)
+                    ship_name.grid(row=i + 6, column=0)
+                    ship_cash.grid(row=i + 6, column=1)
+                    ship_average.grid(row=i + 6, column=2)
+                    ship_trips.grid(row=i + 6, column=3)
 
     def on_screen_view():
+
         # Update the status text field in GUI by removing old message and supplying new
         response_text.delete("1.0", END)
         response_text.insert(END, 'Processing List')
+        ship_name_header = Entry(app, justify=CENTER)
+        ship_cash_header = Entry(app, justify=CENTER)
+        ship_average_header = Entry(app, justify=CENTER)
+        ship_trips_header = Entry(app, justify=CENTER)
+
+        ship_name_header.insert(0, 'Ship Name')
+        ship_cash_header.insert(0, 'Total Credits Earned')
+        ship_average_header.insert(0, 'Average Credits')
+        ship_trips_header.insert(0, 'Total Trips Made')
+
+        ship_name_header.grid(row=5, column=0)
+        ship_cash_header.grid(row=5, column=1)
+        ship_average_header.grid(row=5, column=2)
+        ship_trips_header.grid(row=5, column=3)
         generate_correct_view('onscreen')
 
     # Create widgets
     path_label = Label(app, text='Path to save file')
     browse_button = Button(app, text="Browse", command=browse_function)
     csv_button = Button(app, text='Generate CSV', command=generate_csv)
-    json_button = Button(app, text='On screen view', command=on_screen_view)
+    json_button = Button(app, text='On screen view (WIP)', command=on_screen_view)
     path_actual_entry = Entry(app, width=50)
     response_text = Text(app, height=2, width=50)
 
@@ -117,6 +148,9 @@ def main():
     csv_button.grid(row=3, column=1)
     json_button.grid(row=3, column=2)
     response_text.grid(row=4, column=1)
+
+    # for testing
+    # path_actual_entry.insert(0, 'x4-ship-profit-parser/save_001.xml.gz')
 
     # Initial setup
     response_text.insert(END, 'Results go here')
